440e85e470475e8ce7aa9878c1df9ec98f1f16e1
David AIGRET <DAVIDAIGRET@GMAIL.COM>
2017-12-01 18:20:19 +0100

    Update parite.js

--- chapitre_4/js/parite.js
+++ chapitre_4/js/parite.js
@@ -4,7 +4,7 @@ Exercice : afficher les nombres pairs et impairs
 
 var nombre = Number(prompt("Entrez le nombre initial :"));
 
-for (var i = nombre; i < nombre + 10; i++) {
+for (var i = nombre; i < nombre + 9; i++) {  //ds l'énoncé :Le programme ne doit afficher que 10 nombres (y compris celui saisi) et non 11.
     if (i % 2 === 0) {
         console.log(i + " est pair");
     } else {
